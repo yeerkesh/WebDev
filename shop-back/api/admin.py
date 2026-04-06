d2348ab132e0035e9e651b5080320a1534977e78
Yerkebulan Yessimbek <erkebulanesimbek@gmail.com>
2026-04-06 17:40:35 +0500

    add assignment 9

--- shop-back/api/admin.py
+++ shop-back/api/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
-from .models import Product, Category
-
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "price", "count", "is_active", "category")
-    list_filter = ("is_active", "category")
-    search_fields = ("name",)
+from .models import Category, Product
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("id", "name")
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'count', 'is_active', 'category')
+    list_filter = ('is_active', 'category')
+    search_fields = ('name',)
+    ordering = ('name',)
--- shop-back/api/views.py
+++ shop-back/api/views.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.decorators import action
 from rest_framework.response import Response
+
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
 
@@ -9,14 +10,47 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-    @action(detail=True, methods=['get'])
-    def products(self, request, pk=None):
-        category = self.get_object()
-        products = category.products.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
-
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name']
+    ordering_fields = ['name', 'price']
+    ordering = ['name']
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+
+        category = self.request.query_params.get('category')
+        is_active = self.request.query_params.get('is_active')
+
+        if category:
+            queryset = queryset.filter(category_id=category)
+
+        if is_active is not None:
+            if is_active.lower() == 'true':
+                queryset = queryset.filter(is_active=True)
+            elif is_active.lower() == 'false':
+                queryset = queryset.filter(is_active=False)
+
+        return queryset
+
+    @action(detail=False, methods=['get'])
+    def active(self, request):
+        queryset = Product.objects.filter(is_active=True)
+
+        category = request.query_params.get('category')
+        if category:
+            queryset = queryset.filter(category_id=category)
+
+        search = request.query_params.get('search')
+        if search:
+            queryset = queryset.filter(name__icontains=search)
+
+        ordering = request.query_params.get('ordering')
+        if ordering in ['name', '-name', 'price', '-price']:
+            queryset = queryset.order_by(ordering)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
