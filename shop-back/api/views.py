52d23772954c0b79da8dee7fe2c9274ef42cd6f2
Yerkebulan Yessimbek <erkebulanesimbek@gmail.com>
2026-04-06 15:27:16 +0500

    updating lab8

--- shop-back/api/views.py
+++ shop-back/api/views.py
@@ -1,99 +1,22 @@
-from django.http import JsonResponse
-from .models import Product, Category
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
 
-def product_list(request):
-    products = Product.objects.all()
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
-    category_id = request.GET.get("category")
-    active = request.GET.get("active")
-    search = request.GET.get("search")
-
-    if category_id:
-        products = products.filter(category_id=category_id)
-
-    if active == "true":
-        products = products.filter(is_active=True)
-    elif active == "false":
-        products = products.filter(is_active=False)
-
-    if search:
-        products = products.filter(name__icontains=search)
-
-    data = []
-    for product in products:
-        data.append({
-            'id': product.id,
-            'name': product.name,
-            'price': product.price,
-            'description': product.description,
-            'count': product.count,
-            'is_active': product.is_active,
-            'category_id': product.category.id
-        })
-
-    return JsonResponse(data, safe=False)
-
-
-def product_detail(request, id):
-    try:
-        product = Product.objects.get(id=id)
-        data = {
-            'id': product.id,
-            'name': product.name,
-            'price': product.price,
-            'description': product.description,
-            'count': product.count,
-            'is_active': product.is_active,
-            'category_id': product.category.id
-        }
-        return JsonResponse(data)
-    except Product.DoesNotExist:
-        return JsonResponse({'error': 'Product not found'})
-
-
-def category_list(request):
-    categories = Category.objects.all()
-    data = []
-
-    for category in categories:
-        data.append({
-            'id': category.id,
-            'name': category.name
-        })
-
-    return JsonResponse(data, safe=False)
-
-
-def category_detail(request, id):
-    try:
-        category = Category.objects.get(id=id)
-        data = {
-            'id': category.id,
-            'name': category.name
-        }
-        return JsonResponse(data)
-    except Category.DoesNotExist:
-        return JsonResponse({'error': 'Category not found'})
-
-
-def category_products(request, id):
-    try:
-        category = Category.objects.get(id=id)
+    @action(detail=True, methods=['get'])
+    def products(self, request, pk=None):
+        category = self.get_object()
         products = category.products.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 
-        data = []
-        for product in products:
-            data.append({
-                'id': product.id,
-                'name': product.name,
-                'price': product.price,
-                'description': product.description,
-                'count': product.count,
-                'is_active': product.is_active,
-                'category_id': product.category.id
-            })
 
-        return JsonResponse(data, safe=False)
-    except Category.DoesNotExist:
-        return JsonResponse({'error': 'Category not found'})
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
